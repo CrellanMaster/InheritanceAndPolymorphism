c6074554f8c7de0078f651e854299c12647730d8
Sandro Crellan <devsandrounity@gmail.com>
2022-07-07 00:27:57 -0300

    Class 2 Completed

---
+++ Inheritance/UpCastingDownCasting/Entities/BusinessAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpCastingDownCasting.Entities
+{
+    internal class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loan) : base(number, holder, balance)
+        {
+            LoanLimit = loan;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
