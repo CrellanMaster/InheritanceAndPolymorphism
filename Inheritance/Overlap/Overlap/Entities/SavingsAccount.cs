862eb0a37b1ad9f0672139e8d4633be9dea33a5b
CrellanMaster <devsandrounity@gmail.com>
2022-07-08 14:19:14 -0300

    Sealed classes and methods

---
+++ Inheritance/Overlap/Overlap/Entities/SavingsAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overlap.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interest) : base(number, holder, balance)
+        {
+            InterestRate = interest;
+        }
+
+        public void UpdateBalance(double rate)
+        {
+            Balance += Balance * rate;
+        }
+
+        public sealed override void Withdraw(double amount)
+        {
+            base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+
+    }
+}
